3e22f771876cda2878c9d990b8fb810f0e1ed588
Unknown <ruben.ojed9a3@gmail.com>
2018-07-15 17:06:04 -0500

    Fragments VIII
    Se creo el hero adapter

---
+++ app/src/main/java/com/aldominium/marvelheroes/HeroAdapter.java
@@ -0,0 +1,56 @@
+package com.aldominium.marvelheroes;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.aldominium.marvelheroes.Models.SuperHero;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.MyViewHoder>{
+
+    ArrayList<SuperHero> superHeroArrayList;
+    Context context;
+
+    public HeroAdapter(ArrayList superHeroArrayList, Context context){
+        this.superHeroArrayList = superHeroArrayList;
+        this.context = context;
+    }
+
+    @Override
+    public MyViewHoder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.hero_list_item, parent, false);
+        return new MyViewHoder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHoder holder, int position) {
+        SuperHero superHero = superHeroArrayList.get(position);
+        holder.heroDetailNameTextView.setText(superHero.getName());
+        //holder.heroPictureImageView.setImageResource(superHero.getThumbnail());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return superHeroArrayList.size();
+    }
+
+    public class MyViewHoder extends RecyclerView.ViewHolder{
+
+        public ImageView heroPictureImageView;
+        public TextView heroDetailNameTextView;
+
+        public MyViewHoder(View itemView) {
+            super(itemView);
+
+            heroPictureImageView = itemView.findViewById(R.id.heroPictureImageView);
+            heroDetailNameTextView = itemView.findViewById(R.id.heroDetailNameTextView);
+        }
+    }
+}
